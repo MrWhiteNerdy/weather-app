0840878deb3f32ce0d0de0bfb4a0a3a966bcf9d5
Joseph Davis <joseph.n.davis.98@gmail.com>
2016-06-22 19:44:32 -0500

    Make background image responsive

--- weather-app.js
+++ weather-app.js
@@ -14,10 +14,10 @@ function getLocation() {
 
 function showPosition(position) {
   var latitude, longitude;
-  
+
   latitude = position.coords.latitude;
   longitude = position.coords.longitude;
-  
+
   getWeatherData(latitude, longitude);
 }
 
@@ -44,62 +44,62 @@ function getWeatherData(latitude, longitude) {
 function changeIcon(icon) {
     switch (icon) {
         case "01d":
-            $("body").css("background-image", "url('images/clear-sky-day.jpg')");
+            $("html").css("background-image", "url('images/clear-sky-day.jpg')");
             break;
         case "01n":
-            $("body").css("background-image", "url('images/clear-sky-night.jpg')");
+            $("html").css("background-image", "url('images/clear-sky-night.jpg')");
             break;
         case "02d":
-            $("body").css("background-image", "url('images/few-clouds-day.jpg')");
+            $("html").css("background-image", "url('images/few-clouds-day.jpg')");
             break;
         case "02n":
-            $("body").css("background-image", "url('images/few-clouds-night.jpg')");
+            $("html").css("background-image", "url('images/few-clouds-night.jpg')");
             break;
         case "03d":
-            $("body").css("background-image", "url('images/few-clouds-day.jpg')");
+            $("html").css("background-image", "url('images/few-clouds-day.jpg')");
             break;
         case "03n":
-            $("body").css("background-image", "url('images/few-clouds-night.jpg')");
+            $("html").css("background-image", "url('images/few-clouds-night.jpg')");
             break;
         case "04d":
-            $("body").css("background-image", "url('images/few-clouds-day.jpg')");
+            $("html").css("background-image", "url('images/few-clouds-day.jpg')");
             break;
         case "04n":
-            $("body").css("background-image", "url('images/few-clouds-night.jpg')");
+            $("html").css("background-image", "url('images/few-clouds-night.jpg')");
             break;
         case "09d":
-            $("body").css("background-image", "url('images/rainy-day.jpg')");
+            $("html").css("background-image", "url('images/rainy-day.jpg')");
             break;
         case "09n":
-            $("body").css("background-image", "url('images/rainy-night.jpg')");
+            $("html").css("background-image", "url('images/rainy-night.jpg')");
             break;
         case "10d":
-            $("body").css("background-image", "url('images/rainy-day.jpg')");
+            $("html").css("background-image", "url('images/rainy-day.jpg')");
             break;
         case "10n":
-            $("body").css("background-image", "url('images/rainy-night.jpg')");
+            $("html").css("background-image", "url('images/rainy-night.jpg')");
             break;
         case "11d":
-            $("body").css("background-image", "url('images/thunderstorm-day.jpg')");
+            $("html").css("background-image", "url('images/thunderstorm-day.jpg')");
             break;
         case "11n":
-            $("body").css("background-image", "url('images/thunderstorm-night.jpg')");
+            $("html").css("background-image", "url('images/thunderstorm-night.jpg')");
             break;
         case "13d":
-            $("body").css("background-image", "url('images/snowy-day.jpg')");
+            $("html").css("background-image", "url('images/snowy-day.jpg')");
             $(".big-text").css("color", "#1F3E9A");
             $(".summary").css("color", "#1F3E9A");
             break;
         case "13n":
-            $("body").css("background-image", "url('images/snowy-night.jpg')");
+            $("html").css("background-image", "url('images/snowy-night.jpg')");
             break;
         case "50d":
-            $("body").css("background-image", "url('images/misty-day.jpg')");
+            $("html").css("background-image", "url('images/misty-day.jpg')");
             $(".big-text").css("color", "#1F3E9A");
             $(".summary").css("color", "#1F3E9A");
             break;
         case "50n":
-            $("body").css("background-image", "url('images/misty-night.jpg')");
+            $("html").css("background-image", "url('images/misty-night.jpg')");
             break;
     }
 }

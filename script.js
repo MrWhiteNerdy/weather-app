3a548b443e5373b54df728c68fcfa5b269915a46
Joseph Davis <soccermaniac898@gmail.com>
2017-09-12 17:50:17 -0500

    Update UI

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-$(window).load(function(){
-	$(".loader").fadeOut("slow");
+$(window).load(function () {
+    $(".loader").fadeOut("slow");
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
     initialize();
     getLocation();
-    changeIcon();
     setInterval(getLocation, 300000);
 });
 
-var geocoder;
-var city;
+let geocoder;
+let city;
 
 function initialize() {
     geocoder = new google.maps.Geocoder();
 }
 
 function getCity(lat, lng) {
-    var latlng = new google.maps.LatLng(lat, lng);
-    geocoder.geocode({'latLng': latlng}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-          if (results[1]) {
-              var arrAddress = results;
-              $.each(arrAddress, function(i, address_component) {
-                  if (address_component.types[0] == "locality") {
-                      city = address_component.address_components[0].long_name;
+    let latlng = new google.maps.LatLng(lat, lng);
+    geocoder.geocode({'latLng': latlng}, function (results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            if (results[1]) {
+                $.each(results, function (i, address_component) {
+                    if (address_component.types[0] === "locality") {
+                        city = address_component.address_components[0].long_name;
                     }
                 });
             } else {
@@ -45,73 +43,29 @@ function getLocation() {
 }
 
 function showPosition(position) {
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
 
     getCity(latitude, longitude);
     getWeatherData(latitude, longitude);
 }
 
 function getWeatherData(latitude, longitude) {
-    var temp;
-    var summary;
-    var icon;
-
     $.ajax({
         url: "https://api.darksky.net/forecast/9bb326b6d66740ee5c8b8f8dd4091d69/" + latitude + "," + longitude,
         dataType: "jsonp",
         jsonp: "callback",
-        success: function(data) {
-            temp = Math.round(data.currently.temperature);
-            summary = data.currently.summary;
-            icon = data.currently.icon;
-
-            changeIcon(icon);
-
+        success: function (data) {
             $(".location").html(city);
-            $(".temperature").html(temp + "&deg; F");
-            $(".summary").html(summary);
+            $(".temperature").html(Math.round(data.currently.temperature) + "&deg; F");
+            $(".summary").html(data.currently.summary);
+            setIcon(data.currently.icon);
         }
-    })
+    });
 }
 
-function changeIcon(icon) {
-    switch (icon) {
-        case "clear-day":
-            $("html").css("background-image", "url('images/clear-sky-day.jpg')");
-            break;
-        case "clear-night":
-            $("html").css("background-image", "url('images/clear-sky-night.jpg')");
-            break;
-        case "partly-cloudy-day":
-            $("html").css("background-image", "url('images/few-clouds-day.jpg')");
-            $(".big-text").css("color", "#191432");
-            $(".small-text").css("color", "#191432");
-            break;
-        case "partly-cloudy-night":
-            $("html").css("background-image", "url('images/few-clouds-night.jpg')");
-            break;
-        case "sleet":
-            $("html").css("background-image", "url('images/sleet.jpg')");
-            break;
-        case "wind":
-            $("html").css("background-image", "url('images/wind.jpg')");
-            break;
-        case "fog":
-            $("html").css("background-image", "url('images/fog.jpg')");
-            break;
-        case "cloudy":
-            $("html").css("background-image", "url('images/cloudy.jpg')");
-            break;
-        case "rain":
-            $("html").css("background-image", "url('images/rainy-day.jpg')");
-            break;
-        case "snow":
-            $("html").css("background-image", "url('images/snowy-day.jpg')");
-            $(".big-text").css("color", "#1F3E9A");
-            $(".small-text").css("color", "#1F3E9A");
-            break;
-        default:
-            
-    }
+function setIcon(icon) {
+    let skycons = new Skycons();
+    skycons.add("icon", icon);
+    skycons.play();
 }
